cbe91cd67e3604237952086d65ae9c0c22688f45
leonardpoppenborg <leonard.poppenborg@tum.de>
2025-01-16 14:54:38 +0100

    Add Animation and update logic to class Bomb

--- core/src/de/tum/cit/ase/bomberquest/gameobjects/Bomb.java
+++ core/src/de/tum/cit/ase/bomberquest/gameobjects/Bomb.java
@@ -6,32 +6,78 @@ import de.tum.cit.ase.bomberquest.map.Coordinates;
 import de.tum.cit.ase.bomberquest.texture.Animations;
 import de.tum.cit.ase.bomberquest.texture.Textures;
 
-import java.util.Comparator;
-
 public class Bomb extends Coordinates {
 
     private float elapsedTime;
+    private float ignitingAnimationTime;
+    private float explosionAnimationTime;
+    private boolean isExplosionAnimationPlaying;
+    private float explosionDelay;
+    private boolean exploded;
+
+    /**
+     *
+     * @param x x-coordinate of the Bomb
+     * @param y y-coordinate of the Bomb
+     * @param explosionDelay Time between the bomb has been dropped and the actual explosion
+     *
+     *
+     */
 
 
-    public Bomb(float x, float y) {
+    public Bomb(float x, float y, float explosionDelay) {
         super(x, y);
         this.elapsedTime = 0;
+        this.ignitingAnimationTime = 0;
+        this.explosionAnimationTime = 0;
+        this.isExplosionAnimationPlaying = false;
+        this.explosionDelay= explosionDelay;
+        this.exploded = false;
+
     }
 
     //Methods
 
-    public TextureRegion getCurrentAppearance() {
-        return null;
+    /**
+     * Method for the bombs texture
+     * @return
+     */
+
+    public TextureRegion getCurrentAppearance () {
+        if (!exploded) {
+            return Animations.BOMB_IGNITING.getKeyFrame(elapsedTime, true);
+        } else if (isExplosionAnimationPlaying) {
+            return Animations.BOMB_Exploding.getKeyFrame(explosionAnimationTime, false);
+        }
+        else return null;
     }
 
 
 
     public void explode() {
+        exploded = true;
+        isExplosionAnimationPlaying = true;
 
     }
 
+    /**
+     * Method to update the elapsed Time, ignitingAnimationTime, explosionAnimationTime;
+     * @param deltaTime time of the game;
+     */
+
     public void update(float deltaTime) {
-        elapsedTime = deltaTime;
+        if (!exploded) {
+            elapsedTime += deltaTime;
+            ignitingAnimationTime += deltaTime;
+            if (elapsedTime >= explosionDelay) {
+                explode();
+            }
+        } else if (isExplosionAnimationPlaying) {
+            explosionAnimationTime += deltaTime;
+            if (Animations.BOMB_Exploding.isAnimationFinished(explosionAnimationTime)) {
+                isExplosionAnimationPlaying = false;
+            }
+        }
     }
 
 
@@ -41,4 +87,11 @@ public class Bomb extends Coordinates {
         return elapsedTime;
     }
 
+    public float getExplosionDelay() {
+        return explosionDelay;
+    }
+
+    public boolean isExploded() {
+        return exploded;
+    }
 }
--- core/src/de/tum/cit/ase/bomberquest/texture/Animations.java
+++ core/src/de/tum/cit/ase/bomberquest/texture/Animations.java
@@ -110,5 +110,13 @@ public class Animations {
             SpriteSheet.ORIGINAL_BOMBERMAN.at(4, 1)
     );
 
-    
+    /**
+     * The animation for the bomb exploding;
+     */
+
+    public static final Animation<TextureRegion> BOMB_Exploding = new Animation<>(0.1f,
+            SpriteSheet.ORIGINAL_BOMBERMAN.at(4, 3),
+            SpriteSheet.ORIGINAL_BOMBERMAN.at(4, 2),
+            SpriteSheet.ORIGINAL_BOMBERMAN.at(4, 1)
+    );
 }
