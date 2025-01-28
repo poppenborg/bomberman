93c1e0c1dfbd62e1ae17f35f1ad269aa06ea04b9
leonardpoppenborg <leonard.poppenborg@tum.de>
2025-01-28 20:21:14 +0100

    Set the game time to 3 minutes.

--- core/src/de/tum/cit/ase/bomberquest/map/GameMap.java
+++ core/src/de/tum/cit/ase/bomberquest/map/GameMap.java
@@ -100,7 +100,7 @@ public class GameMap {
     public GameMap(BomberQuestGame game, FileHandle mapFile) {
         this.game = game;
         this.mapFile = mapFile;
-        this.timeLeft = 120;
+        this.timeLeft = 180;
         this.world = new World(Vector2.Zero, true);
         this.contactListener = new GameContactListener();
         world.setContactListener(contactListener);
